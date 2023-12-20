a50eb61428a3f1dad5cb2bb2219617239605ae21
Nico Rodriguez <nicolasrodriguez.mza@gmail.com>
2023-12-20 20:52:21 -0300

    Segunda pre entrega

--- js/index.js
+++ js/index.js
@@ -1,57 +1,95 @@
 
-console.log("Bienvenido al programa de conversion.")
-console.log(" ")
+console.log("LISTADO DE PELICULAS DISPONIBLES.");
 
+let estado = true;
 
-let opc; 
+let peliculaid = 1;
+let peliculas = [];
 
-
-
-do{
-
-console.log("1_ Dolar. 0,0028 usd  ");
-console.log("2_ Euro.  0,0025 eur ");
-console.log("3_ Yen.   0,41 jpy");
-console.log("4_ Real.  0,14 brl ");
-console.log("5_ Salir.");
-
-
-let pesos = prompt("Ingrese el monto que desea saber la conversion");
-
-opc = parseInt(prompt("Ingrese la opcion del tipo de moneda que desea convertir."));
-
-switch(opc){
-
-    case 1: console.log(" La conversion a dolares es: " + converDolar(pesos) );
-    break;
-
-    case 2: console.log(" La conversion a euros es: " + converEuro(pesos));
-    break;
-
-    case 3: console.log(" La conversion a yenes es: " + converYenes(pesos));
-    break;
-
-    case 4: console.log(" La conversion a reales es: " + converReal(pesos));
-    break;
-
-    case 5: console.log("Muchas Gracias!");
-    break;
-
-    default: console.log("Ingrese una opcion valida.");
-}
-}while(opc !=5);
-
-function converDolar (pesos){
-    return pesos*0.0028;
+class Pelicula{
+    constructor(nombre){
+        this.nombre=nombre;
+        this.id = peliculaid;    
+    }
 }
 
-function converEuro (pesos){
-    return pesos*0.0025;
+const agregarPelicula = (pelicula) => {
+    peliculas.push(pelicula);
+    peliculaid ++;
+
 }
-function converYenes (pesos){
-    return pesos*0.41;
+
+const eliminarPelicula = (id)=>{
+
+    let existe = peliculas.some( pelicula => pelicula.id == id);
+
+    if(existe){
+        peliculas = peliculas.filter(pelicula => pelicula.id !== id);
+        alert("Se ha eliminado con exito!");
+    }else{
+        alert("No exite pelicula con ese ID.")
+    }
 }
-function converReal (pesos){
-    return pesos*0.14;
+
+const mostrarPelicula = () => {
+    let nombresPeliculas = peliculas.map( pelicula => pelicula.id + " - Nombre: " + pelicula.nombre);
+
+    if(nombresPeliculas.length > 0){
+        alert(nombresPeliculas.join("\n"));
+    }else{
+        alert("No hay Peliculas agregadas.");
+    }
+ 
 }
+
+const buscarPelicula = (id) => {
+
+    let buscar = peliculas.some(pelicula => pelicula.id == id);
+
+    if(buscar){
+        peliculas = peliculas.find( pelicula => pelicula.id !== id);º
+
+    }
+}
+
+const menuPrincipal = () => {
+
+    while(estado){
+
+   let opc = parseInt(prompt('Ingrese una opcion.'));
+   console.log("1_ Mostrar Peliculas En grilla.");  
+   console.log("2_ Agregar pelicula.");
+   console.log("3_ Eliminar Pelicula.");
+   console.log("4_ Salir.");
+
+   switch(opc){
+        case 1: mostrarPelicula();
+        break;
+
+        case 2: 
+        let nombre = prompt("Ingrese el Nombre.");
+        
+        let pelicula = new Pelicula(nombre);
+ 
+        agregarPelicula(pelicula);
+        
+        break;
+
+        case 3:
+            let idpeliculas = parseInt(prompt("Ingrese el ID de la Pelicula a eliminar."));
+            eliminarPelicula(idpeliculas);
+
+        break;
+
+        case 4:
+            console.log("Muchas Gracias!");
+            estado=false;
+        break;
+    
+   }
+}
+
+};
+
+menuPrincipal();
 
